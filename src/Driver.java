569dab7a7f60646d19d8141fe1a6d5db23c54c0a
iRich <boundtodoom@gmail.com>
2016-12-09 21:23:18 -0500

    still figuring out intellij VCS

---
+++ src/Driver.java
@@ -0,0 +1,11 @@
+/**
+ * Created by usr on 12/9/2016.
+ *
+ *
+ */
+public class Driver {
+    public static void main(String[] args){
+        System.out.println("Hello, Github!");
+        System.out.println("github test");
+    }
+}
